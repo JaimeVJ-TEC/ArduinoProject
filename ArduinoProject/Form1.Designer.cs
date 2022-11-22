07cd0608b68e1dbb290191ab8aac9595a42e4c10
JaimeVJ-TEC <L19100265@nlaredo.tecnm.mx>
2022-11-21 21:57:27 -0600

    Add project files.

---
+++ ArduinoProject/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace ArduinoProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.lblBPM = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.lblNofinger = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(655, 37);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.Location = new System.Drawing.Point(655, 66);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(75, 23);
+            this.btnDetener.TabIndex = 1;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = true;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // lblBPM
+            // 
+            this.lblBPM.AutoSize = true;
+            this.lblBPM.Location = new System.Drawing.Point(106, 37);
+            this.lblBPM.Name = "lblBPM";
+            this.lblBPM.Size = new System.Drawing.Size(35, 13);
+            this.lblBPM.TabIndex = 2;
+            this.lblBPM.Text = "label1";
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // lblNofinger
+            // 
+            this.lblNofinger.AutoSize = true;
+            this.lblNofinger.Location = new System.Drawing.Point(106, 149);
+            this.lblNofinger.Name = "lblNofinger";
+            this.lblNofinger.Size = new System.Drawing.Size(147, 13);
+            this.lblNofinger.TabIndex = 3;
+            this.lblNofinger.Text = "Coloque su dedo en el sensor";
+            this.lblNofinger.Visible = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 316);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(485, 96);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.lblNofinger);
+            this.Controls.Add(this.lblBPM);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnIniciar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.Label lblBPM;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label lblNofinger;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ ArduinoProject/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ArduinoProject
+{
+    public partial class Form1 : Form
+    {
+        SerialPort Arduino;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Arduino = new SerialPort();
+            Arduino.PortName = "COM3";
+            Arduino.BaudRate = 115200;
+            timer.Interval = 20;
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            Arduino.Open();
+            timer.Start();
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            CerrarPuerto();
+        }
+
+        void CerrarPuerto()
+        {
+            if (Arduino.IsOpen)
+            {
+                Arduino.Close();
+                timer.Stop();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CerrarPuerto();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            string Line = Arduino.ReadLine();
+            richTextBox1.Text += Line;
+
+            switch(Line)
+            {
+                case "No finger":
+                    lblNofinger.Text = "Coloque su dedo en el sensor";
+                    lblNofinger.Visible = true;
+                    break;
+
+                case "NO SENSOR":
+                    lblNofinger.Text = "Sensor no encontrado";
+                    lblNofinger.Visible = true;
+                    break;
+
+                case "BEAT":
+                    lblNofinger.Visible = false;
+                    break;
+
+                default:
+                    if(Line.Contains('|'))
+                    {
+                        string[] resultados = Line.Split('|');
+                        lblBPM.Text = resultados[1];
+                        lblNofinger.Visible = false;
+                    }
+                    break;
+            }
+        }
+    }
+}
